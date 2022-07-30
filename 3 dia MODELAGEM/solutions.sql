e6b8f1232ba279df7b104e6c596b02ab9974a9c9
GabsRRC <gabriellerrc@gmail.com>
2022-07-29 21:22:29 -0300

    fixx: virgulas foram esquecidas

--- 3 dia MODELAGEM/solutions.sql
+++ 3 dia MODELAGEM/solutions.sql
@@ -1,66 +1,65 @@
 CREATE TABLE states (
 	id SERIAL PRIMARY KEY,
-	name TEXT VARCHAR(50) UNIQUE NOT NULL
+	name TEXT UNIQUE NOT NULL
 )
 
 CREATE TABLE cities (
 	id SERIAL PRIMARY KEY,
-	name TEXT NOT NULL VARCHAR(50) UNIQUE
-	"stateId" INTEGER NOT NULL REFERENCES "state"("id")
+	name TEXT NOT NULL UNIQUE,
+	"stateId" integer REFERENCES states(id)
 )
 
 CREATE TABLE customerPhones (
 	id SERIAL PRIMARY KEY,
-    "customerId" INTEGER NOT NULL REFERENCES "customer"("id")
-    number TEXT VARCHAR(12)
-    type ENUM('landline', 'mobile') --landline or mobile
+    "customerId" INTEGER NOT NULL REFERENCES "customers"("id"),
+    number TEXT,
+    type TEXT --landline or mobile
 )
 
 CREATE TABLE customerAddresses ( -- user so pode ter um endereço
 	id SERIAL PRIMARY KEY,
-	"customerId" INTEGER UNIQUE NOT NULL REFERENCES "customer"("id")
-    street TEXT 
-    number TEXT VARCHAR(12)
-    complement TEXT
-    "postalCode" TEXT VARCHAR(12)
+	"customerId" INTEGER UNIQUE NOT NULL REFERENCES "customers"("id"),
+    street TEXT,
+    number TEXT, 
+    complement TEXT,
+    "postalCode" TEXT,
     "cityId" INTEGER NOT NULL REFERENCES "cities"("id")
 )
 
 CREATE TABLE customers (
 	id SERIAL PRIMARY KEY,
-	"fullName" TEXT
-    cpf VARCHAR(12)
-    email TEXT
+	"fullName" TEXT,
+    cpf TEXT,
+    email TEXT,
     password TEXT
 )
 
 CREATE TABLE bankAccount (
 	id SERIAL PRIMARY KEY,
-	"customerId" INTEGER NOT NULL REFERENCES "customer"("id")
-    "accountNumber" INTEGER
-    agency INTEGER
-    "openDate" DATE
+	"customerId" INTEGER NOT NULL REFERENCES "customers"("id"),
+    "accountNumber" INTEGER,
+    agency INTEGER,
+    "openDate" DATE,
     "closeDate" DATE
 )
 
 CREATE TABLE transactions (
 	id SERIAL PRIMARY KEY,
-	"bankAccountId" INTEGER NOT NULL REFERENCES "bankAccount"("id")
-    amount NUMERIC
-    type ENUM('deposit', 'withdraw') --deposit or withdraw
-    time TIMESTAMP
-    description TEXT
+	"bankAccountId" INTEGER NOT NULL REFERENCES bankAccount("id"),
+    amount NUMERIC,
+    type TEXT, --deposit or withdraw
+    time TIMESTAMP,
+    description TEXT,
     cancelled BOOLEAN 
 )
 
 CREATE TABLE creditCards (
 	id SERIAL PRIMARY KEY,
-	"bankAccountId" INTEGER NOT NULL REFERENCES "bankAccount"("id")
-    name TEXT
-    number INTEGER
-    "securityCode" INTEGER
-    "expirationMonth" DATE
-    "expirationYear" DATE
+	"bankAccountId" INTEGER NOT NULL REFERENCES bankAccount("id"),
+    name TEXT,
+    number INTEGER,
+    "securityCode" INTEGER,
+    "expirationMonth" DATE,
+    "expirationYear" DATE,
     password TEXT
-    limit NUMERIC
 )
